2b9f76213905db003e59290991eb046f78bb9161
AgnesGrace <agnesaugustine42@gmail.com>
2023-05-20 13:39:33 +0200

    feat[mvc/model]:created the database PDO class, connected to db, added a model and linked it in the controller

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,8 @@
 <?php require APPROOT . '/views/includes/header.php';?>
 <h1><?php echo $data['title'];?></h1>
+<h3>Hello ⭐️ this is the AgmercyMVC PHP framework, built from scratch</h3>
+<h4>Understanding how this works will help you to easily understand how any <span>PHP framwork</span> works</h4>
+<P>Kindly visit the documentation on how to use it</P>
 <ul>
     <?php foreach($data['post'] as $post):?>
         <li><?php echo $post->title; ?></li>
